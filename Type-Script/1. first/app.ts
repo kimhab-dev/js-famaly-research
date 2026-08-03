9dd32ac2f559b37a10c95bb00f0ceb27d72adf6c
kimhab <kimhabsok9@gmail.com>
2026-08-03 14:43:29 +0700

    first commit

---
+++ Type-Script/1. first/app.ts
@@ -0,0 +1,16 @@
+// let message: string = "Hello from MPTC."
+// let message2: string = "Hahaha";
+// console.log(message);
+// console.log(message2);
+
+// let fisrtname: string = "kimhab";
+// let age: number = 22;
+// let isStudent: boolean = false;
+
+// console.log("first name is : ", fisrtname);
+// console.log("age : ", age);
+// console.log("isStudent : ", isStudent);
+
+
+
+
